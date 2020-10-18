13abee5c464b9de07e6eb4c94fd36fae0e32a926
Kim Kern <github@kimkern.de>
2020-10-19 00:18:24 +0200

    fix backend service setup

--- apps/deployment/src/app/backend-setup.ts
+++ apps/deployment/src/app/backend-setup.ts
@@ -2,15 +2,19 @@ import { exec } from './exec-promise';
 import * as ora from 'ora';
 
 export async function setupBackend(domain, port) {
-  const spinner = ora().start('Setting up backend');
+  const spinner = ora().start('Building backend');
+  await exec(`npm run nx build api -- --maxWorkers 1 --memoryLimit 1000`);
+  spinner.succeed('Backend built');
+
+  spinner.start('Setting up backend as daemon');
   await exec(
-    `echo "[program:qmailo]\ncommand=npm run nx serve api --prefix /home/${process.env.USER}/qmailo 2>&1\nautostart=yes\nautorestart=yes" >> /home/${process.env.USER}/etc/service.d/qmailo.ini`,
+    `echo -en "[program:qmailo]\\ncommand=node/home/${process.env.USER}/qmailo/dist/apps/api/main.js 2>&1\\nautostart=yes\\nautorestart=yes" >> /home/${process.env.USER}/etc/service.d/qmailo.ini`,
   );
   await exec(`supervisorctl reread`);
   await exec(`supervisorctl update`);
   await exec(
     `uberspace web backend set qmailo.${domain}/api --remove-prefix --http --port ${port}`,
   );
-  spinner.succeed('Backend started');
+  spinner.succeed('Backend daemon started');
   spinner.stop();
 }
